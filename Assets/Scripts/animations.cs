d11ba42c1fadd9423e98a8ac670598c2de828690
Schwartz Marine <m.schwartz@ludus-academie.com>
2020-04-15 20:05:26 +0200

    additions au script
    début de l'écriture du script, essais, on n'y est pas encore et ça marche pas D: mais ça va bien se passer o/
    
    vidéo intéressante pour l'intégration de l'Animator dans le script:
    https://www.youtube.com/watch?v=Xfc9cOoe4vM&list=PLUWxWDlz8PYKnrd27LTqOxL2lr3KhEVRT&index=4

--- Assets/Scripts/animations.cs
+++ Assets/Scripts/animations.cs
@@ -2,17 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class animations : MonoBehaviour
+public class Animations : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool monstreGauche; // d'abord false; booleen devient true quand le monstre va sortir
+    private bool monstreDroite; // d'abord false; devient true quand le monstre va sortir
+
+    private bool enfantCouverture; // OPTIONNEL ; devient aléatoirement true pour déclencher petite animation, puis repasse à false
+
+    public Animator animator;
+
     void Start()
     {
-        
+        Debug.Log("game start");
+
+        monstreGauche = false;
+        monstreDroite = false;
+
+        enfantCouverture = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
+
+        MonstresOnOff();
+ 
+        if (monstreDroite == true)
+        {
+            MonstreDroite();
+        }
         
+
+        if (monstreGauche == true)
+        {
+            MonstreGauche();
+        }
+
+
+        void MonstresOnOff()
+        // les deux conditions suivantes permettent de basculer a true/false les booleens des monstres ; code temporaire en attendant qe pouvoir le faire automatiquement
+        {
+            if (Input.GetKeyDown(KeyCode.D) && (monstreDroite == false))
+            {
+                monstreDroite = true;
+                Debug.Log("monstreDroite = true");
+            }
+            else
+            {
+                monstreDroite = false;
+                Debug.Log("monstreDroite = false");
+            }
+            if (Input.GetKeyDown(KeyCode.G) && (monstreGauche == false))
+            {
+                monstreGauche = true;
+                Debug.Log("monstreGauche = true");
+            }
+            else
+            {
+                monstreGauche = false;
+                Debug.Log("monstreGauche = false");
+            }
+        }
+
+        void MonstreGauche()
+        // !! ne pas confondre avec la variable monstreGauche
+        {
+            if (Input.GetButtonDown("Gauche"))
+            {
+                animator.SetBool("coupGaucheReussi", true);
+            }
+        }
+
+        void MonstreDroite()
+        // !! ne pas confondre avec la variable monstreDroite
+        {
+            if (Input.GetButtonDown("Droite"))
+            {
+                animator.SetBool("coupDroitReussi", true);
+            }
+        }
     }
 }
